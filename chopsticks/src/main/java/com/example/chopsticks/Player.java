127852f59090dcaa648471ac5e373d6649bdf6e0
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-12-15 14:15:25 +0000

    プレイヤーの手の表現をオブジェクトからintに修正

--- chopsticks/src/main/java/com/example/chopsticks/Player.java
+++ chopsticks/src/main/java/com/example/chopsticks/Player.java
@@ -2,12 +2,10 @@ package com.example.chopsticks;
 
 import java.util.Random;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Entity
@@ -22,56 +20,69 @@ public class Player {
     // プレイヤー名
     private String name;
     // 左手
-    @OneToOne(cascade = CascadeType.ALL)
-    private Hand leftHand;
+    private int leftHand;
     // 右手
-    @OneToOne(cascade = CascadeType.ALL)
-    private Hand rightHand;
+    private int rightHand;
 
     public Player(String name) {
         this.name = name;
-        this.leftHand = new Hand();
-        this.rightHand = new Hand();
+        this.leftHand = 1;
+        this.rightHand = 1;
     }
 
     public int getTotalFingerNumber() {
         // 残りの指の数を取得
-        return this.leftHand.getFingerNumber() + this.rightHand.getFingerNumber();
+        return this.leftHand + this.rightHand;
     }
 
-    public Hand show() {
+    public int show() {
         // ランダムに自分の手を選択
         Random rand = new Random();
-        int index = rand.nextInt(2);
+        int handDirection = rand.nextInt(2);
         // 選択された手の残りの指の数を取得
-        Hand selectedHand = index == Player.LEFT ? this.leftHand : this.rightHand;
-        int fingerNumber = selectedHand.getFingerNumber();
-        if (fingerNumber == 0) {
+        int selectedHand = handDirection == Player.LEFT ? this.leftHand : this.rightHand;
+        if (selectedHand == 0) {
             // 選択した手が既に無効になっている場合
             // indexの0, 1をNOTで反転
-            index = ~index;
-            // 再代入
-            selectedHand = index == Player.LEFT ? this.leftHand : this.rightHand;
+            handDirection = ~handDirection;
         }
         // 選択した自分の手を返却
         return selectedHand;
     }
 
-    public Hand select(Hand enemyLeftHand, Hand enemyRightHand) {
+    public int select(Player enemy) {
         // ランダムに相手の手を選択
         Random rand = new Random();
-        int index = rand.nextInt(2);
+        int handDirection = rand.nextInt(2);
         // 選択された手の残りの指の数を取得
-        Hand selectedHand = index == Player.LEFT ? enemyLeftHand : enemyRightHand;
-        int fingerNumber = selectedHand.getFingerNumber();
-        if (fingerNumber == 0) {
+        int selectedHand = handDirection == Player.LEFT ? enemy.getLeftHand() : enemy.getRightHand();
+        if (selectedHand == 0) {
             // 選択した手が既に無効になっている場合
             // indexの0, 1をNOTで反転
-            index = ~index;
-            // 再代入
-            selectedHand = index == Player.LEFT ? enemyLeftHand : enemyRightHand;
+            handDirection = ~handDirection;
         }
         // 選択した相手の手を返却
-        return selectedHand;
+        return handDirection;
+    }
+
+    public void update(int handDirection, int fingerNumber) {
+        // 更新する手を取得
+        int updatedHand = handDirection == Player.LEFT ? this.getLeftHand() : this.getRightHand();
+        // 更新後の指の数を計算
+        int newFingerNumber = updatedHand + fingerNumber;
+        if (newFingerNumber == 5) {
+            // 5本ちょうどなら0本に更新
+            newFingerNumber = 0;
+        } else if (newFingerNumber > 5) {
+            // 5本を超える場合は繰越す
+            newFingerNumber -= 5;
+        }
+        // 4本以下ならそのまま更新
+        // 実際に更新する
+        if (handDirection == Player.LEFT) {
+            this.setLeftHand(newFingerNumber);
+        } else {
+            this.setRightHand(newFingerNumber);
+        }
     }
 }
