58abf19e0813c31487b088137f277c8b7c6c2a4d
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-12-15 13:49:38 +0000

    JPA対応を追加

--- chopsticks/src/main/java/com/example/chopsticks/Hand.java
+++ chopsticks/src/main/java/com/example/chopsticks/Hand.java
@@ -1,14 +1,20 @@
 package com.example.chopsticks;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+@Entity
 @Data
 public class Hand {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int fingerNumber;
 
-    public Hand(int id) {
-        this.id = id;
+    public Hand() {
         this.fingerNumber = 1;
     }
 
--- chopsticks/src/main/java/com/example/chopsticks/Player.java
+++ chopsticks/src/main/java/com/example/chopsticks/Player.java
@@ -2,24 +2,36 @@ package com.example.chopsticks;
 
 import java.util.Random;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
+@Entity
 @Data
 public class Player {
     static final int LEFT = 0;
     static final int RIGHT = 1;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     // プレイヤー名
     private String name;
     // 左手
+    @OneToOne(cascade = CascadeType.ALL)
     private Hand leftHand;
     // 右手
+    @OneToOne(cascade = CascadeType.ALL)
     private Hand rightHand;
 
     public Player(String name) {
         this.name = name;
-        this.leftHand = new Hand(Player.LEFT);
-        this.rightHand = new Hand(Player.RIGHT);
+        this.leftHand = new Hand();
+        this.rightHand = new Hand();
     }
 
     public int getTotalFingerNumber() {
