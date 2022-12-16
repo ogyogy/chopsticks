07a678558c26b11b45fd34e2a5b661adde8b2811
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-12-16 12:14:01 +0000

    コントローラー作成のテストを追加

--- chopsticks/src/test/java/com/example/chopsticks/ChopsticksApplicationTests.java
+++ chopsticks/src/test/java/com/example/chopsticks/ChopsticksApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.chopsticks;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChopsticksApplicationTests {
 
+	@Autowired
+	private ChopsticksController controller;
+
 	@Test
 	void contextLoads() {
+		// コンテキストがコントローラーを作成していることを確認
+		assertThat(controller).isNotNull();
 	}
 
 }
