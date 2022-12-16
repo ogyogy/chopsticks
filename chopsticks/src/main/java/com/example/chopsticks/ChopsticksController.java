6324a0f6df57e96d514d9045db5246f395ec9dea
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-12-16 10:57:59 +0000

    greetingページを削除

--- chopsticks/src/main/java/com/example/chopsticks/ChopsticksController.java
+++ chopsticks/src/main/java/com/example/chopsticks/ChopsticksController.java
@@ -70,11 +70,4 @@ public class ChopsticksController {
         // Viewを返却
         return "result";
     }
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
 }
