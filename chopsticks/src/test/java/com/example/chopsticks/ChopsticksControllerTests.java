795e0837fb72cbeb8ad29a2170ed820e90155a98
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-12-17 04:14:43 +0000

    resultページのテストを追加

--- chopsticks/src/test/java/com/example/chopsticks/ChopsticksControllerTests.java
+++ chopsticks/src/test/java/com/example/chopsticks/ChopsticksControllerTests.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,5 +30,23 @@ public class ChopsticksControllerTests {
                 // Viewに正しいデフォルトパラメータが設定されているか確認
                 .andExpect(model().attribute("player1Name", "Player 1"))
                 .andExpect(model().attribute("player2Name", "Player 2"));
+    }
+
+    @Test
+    public void shouldReturnQueryMessage() throws Exception {
+        // resultページを確認
+        // GETパラメータを定義
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        // NGパターン
+        // GETパラメータを渡していない場合400 Bad Requestが帰ること確認
+        this.mockMvc.perform(get("/result"))
+                .andExpect(status().isBadRequest());
+        // OKパターン
+        // GETパラメータを渡して200 OKが変えることを確認
+        params.add("player1Name", "foo");
+        params.add("player2Name", "bar");
+        this.mockMvc.perform(get("/result").params(
+                params))
+                .andExpect(status().isOk());
     }
 }
